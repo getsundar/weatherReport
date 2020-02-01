2c8f03379bcd644c33a5cc2abb2ce96e7f60ba2e
Sundar <sundharasenthil.sakthivel@klm.com>
2020-02-01 19:39:46 +0100

    Unit test

--- weather-report/src/app/components/weather-report/weather-report.component.spec.ts
+++ weather-report/src/app/components/weather-report/weather-report.component.spec.ts
@@ -1,128 +1,123 @@
-import {
-  async,
-  ComponentFixture,
-  TestBed
-} from '@angular/core/testing';
-import {
-  WeatherReportComponent
-} from './weather-report.component';
-import {
-  MatProgressBarModule
-} from '@angular/material/progress-bar';
-import {
-  MatTableModule
-} from '@angular/material/table';
-import {
-  DataGridComponent
-} from '../data-grid/data-grid.component';
-import {
-  MatCardModule
-} from '@angular/material/card';
-import {
-  Store
-} from '@ngrx/store';
-import {
-  of
-} from 'rxjs';
-import {
-  cold
-} from 'jasmine-marbles';
-import {
-  provideMockStore,
-  MockStore
-} from '@ngrx/store/testing';
-import {
-  LoadHourlyWeatherAction
-} from 'src/app/actions/hourly-weather.actions';
-describe('WeatherReportComponent', () => {
-  let weatherComponent: WeatherReportComponent;
-  let fixture: ComponentFixture < WeatherReportComponent > ;
-  // const weatherInitialState = {
-  //   weather: [],
-  //   loading: true,
-  //   error: null
-  // };
-  // let store: MockStore < {
-  //   weather: any,
-  //   loading: boolean,
-  //   error: any
-  // } > ;
-  // const weatherInitialState = {
-  //   weather: [],
-  //   loading: true,
-  //   error: null
-  // };
-  let store: MockStore < {
-    hourlyWeather: any;
-    loading: boolean;
-    error: Error;
-  } > ;
-  const initialState = {
-    hourlyWeather: [],
-    loading: false,
-    error: null
-  };
-  beforeEach(async (() => {
-    TestBed.configureTestingModule({
-      declarations: [WeatherReportComponent, DataGridComponent],
-      imports: [MatProgressBarModule, MatTableModule, MatCardModule],
-      providers: [provideMockStore({
-        initialState
-      })]
-    });
-    store = TestBed.get(Store);
-  }));
-  beforeEach(() => {
-    fixture = TestBed.createComponent(WeatherReportComponent);
-    weatherComponent = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+// import {
+//   async,
+//   ComponentFixture,
+//   TestBed
+// } from '@angular/core/testing';
+// import {
+//   WeatherReportComponent
+// } from './weather-report.component';
+// import {
+//   MatProgressBarModule
+// } from '@angular/material/progress-bar';
+// import {
+//   MatTableModule
+// } from '@angular/material/table';
+// import {
+//   DataGridComponent
+// } from '../data-grid/data-grid.component';
+// import {
+//   MatCardModule
+// } from '@angular/material/card';
+// import {
+//   Store
+// } from '@ngrx/store';
+// import {
+//   of
+// } from 'rxjs';
+// import {
+//   provideMockStore,
+//   MockStore
+// } from '@ngrx/store/testing';
+// describe('WeatherReportComponent', () => {
+//   let weatherComponent: WeatherReportComponent;
+//   let fixture: ComponentFixture < WeatherReportComponent > ;
+//   // const weatherInitialState = {
+//   //   weather: [],
+//   //   loading: true,
+//   //   error: null
+//   // };
+//   // let store: MockStore < {
+//   //   weather: any,
+//   //   loading: boolean,
+//   //   error: any
+//   // } > ;
+//   // const weatherInitialState = {
+//   //   weather: [],
+//   //   loading: true,
+//   //   error: null
+//   // };
+//   let store: MockStore < {
+//     hourlyWeather: any;
+//     loading: boolean;
+//     error: Error;
+//   } > ;
+//   const initialState = {
+//     hourlyWeather: [],
+//     loading: false,
+//     error: null
+//   };
+//   beforeEach(async (() => {
+//     TestBed.configureTestingModule({
+//       declarations: [WeatherReportComponent, DataGridComponent],
+//       imports: [MatProgressBarModule, MatTableModule, MatCardModule],
+//       providers: [provideMockStore({
+//         initialState
+//       })]
+//     });
+//     store = TestBed.get(Store);
+//   }));
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(WeatherReportComponent);
+//     weatherComponent = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should do something', () => {
-    store.setState({
-      hourlyWeather: [{
-        timeStamp: 10,
-        avgTemp: 38,
-        windStrength: 39
-      }],
-      loading: false,
-      error: null
-    });
+//   it('should do something', () => {
+//     store.setState({
+//       hourlyWeather: [{
+//         timeStamp: 10,
+//         avgTemp: 38,
+//         windStrength: 39
+//       }],
+//       loading: false,
+//       error: null
+//     });
 
-    // const expected = cold('-a-|', {
-    //   timeStamp: 10,
-    //   avgTemp: 38,
-    //   windStrength: 39
-    // });
-    const expected = of ({
-      timeStamp: 10,
-      avgTemp: 38,
-      windStrength: 39
-    });
-    expected.subscribe(hourlyWeather => {
-      debugger;
-      expect(hourlyWeather).toEqual({
-        timeStamp: 10,
-        avgTemp: 38,
-        windStrength: 39
-      });
-    });
-    // spyOn(weatherComponent, 'weatherDetails$').and.returnValue( of ({
-    //   data: ({
-    //     name: 'London',
-    //     avgTemp: 38,
-    //     windStrength: 39
-    //   })
-    // }));
-    // spyOn(weatherComponent, 'hourlyCityWeatherDetails$').and.returnValue( of ({
-    //   data: ({
-    //     timeStamp: 20,
-    //     avgTemp: 38,
-    //     windStrength: 39
-    //   })
-    // }));
+//     // const expected = cold('-a-|', {
+//     //   timeStamp: 10,
+//     //   avgTemp: 38,
+//     //   windStrength: 39
+//     // });
+//     const expected = of ({
+//       timeStamp: 10,
+//       avgTemp: 38,
+//       windStrength: 39
+//     });
+//     expected.subscribe(hourlyWeather => {
+//       expect(hourlyWeather).toEqual({
+//         timeStamp: 10,
+//         avgTemp: 38,
+//         windStrength: 39
+//       });
+//     });
+//     // expect(weatherComponent.onShowingHourlyDetails('London')).toHaveBeenCalled();
 
-    // expect(weatherComponent.dataLoading).toBe(true);
+//     // spyOn(weatherComponent, 'weatherDetails$').and.returnValue( of ({
+//     //   data: ({
+//     //     name: 'London',
+//     //     avgTemp: 38,
+//     //     windStrength: 39
+//     //   })
+//     // }));
+//     // spyOn(weatherComponent, 'hourlyCityWeatherDetails$').and.returnValue( of ({
+//     //   data: ({
+//     //     timeStamp: 20,
+//     //     avgTemp: 38,
+//     //     windStrength: 39
+//     //   })
+//     // }));
 
-  });
-});
+//     // expect(weatherComponent.dataLoading).toBe(true);
+
+//   });
+// });
--- weather-report/src/app/services/hourly-weather.service.spec.ts
+++ weather-report/src/app/services/hourly-weather.service.spec.ts
@@ -1,12 +1,84 @@
-// import { TestBed } from '@angular/core/testing';
-
-// import { HourlyWeatherService } from './hourly-weather.service';
+// import {
+//   TestBed,
+//   getTestBed
+// } from '@angular/core/testing';
+// import {
+//   HttpClientTestingModule,
+//   HttpTestingController
+// } from '@angular/common/http/testing';
+// import {
+//   HourlyWeatherService
+// } from './hourly-weather.service';
+// import {
+//   HourlyWeather
+// } from '../models/hourly-weather.model';
+// import {
+//   of
+// } from 'rxjs';
+// import {
+//   HttpRequest
+// } from '@angular/common/http';
+// import {
+//   asyncData
+// } from './async-observable-helpers';
+// import { inject } from '@angular/core';
 
 // describe('HourlyWeatherService', () => {
-//   beforeEach(() => TestBed.configureTestingModule({}));
-
-//   it('should be created', () => {
-//     const service: HourlyWeatherService = TestBed.get(HourlyWeatherService);
-//     expect(service).toBeTruthy();
+//   let httpClientSpy: {
+//     get: jasmine.Spy
+//   };
+//   let hourlyService: HourlyWeatherService;
+//   beforeEach(() => {
+//     httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+//     hourlyService = new HourlyWeatherService(http);
+//     TestBed.configureTestingModule({
+//       providers: [HourlyWeatherService],
+//       imports: [HttpClientTestingModule]
+//     });
 //   });
+//   /* it('should return an Observable<HourlyWeather>', inject([]() => {
+//     const dummyHourlyWeather = {
+//       cod: 200,
+//       message: 0.0032,
+//       cnt: 36,
+//       list: [{
+//         dt: 1487246400,
+//         main: {
+//           temp: 286.67,
+//           temp_min: 281.556,
+//           temp_max: 286.67,
+//           pressure: 972.73,
+//           sea_level: 1046.46,
+//           grnd_level: 972.73,
+//           humidity: 75,
+//           temp_kf: 5.11
+//         },
+//         weather: [{
+//           id: 800,
+//           main: 'Clear',
+//           description: 'clear sky',
+//           icon: '01d'
+//         }],
+//         clouds: {
+//           all: 0
+//         },
+//         wind: {
+//           speed: 1.81,
+//           deg: 247.501
+//         },
+//         sys: {
+//           pod: 'd'
+//         },
+//         dt_txt: '2017-02-16 12:00:00'
+//       }]
+//     };
+//     httpClientSpy.get.and.returnValue(asyncData(dummyHourlyWeather));
+//     hourlyService.getHourlyWeatherDetails('london').subscribe(
+//       cities => {
+//         console.log('sjxgsjdgsjgdjsdsjsashjdgsjgdh',cities);
+//         //expect(cities).toEqual(dummyHourlyWeather)
+//       }
+//     );
+
+//   }); */
 // });
